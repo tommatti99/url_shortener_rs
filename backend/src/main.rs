9964976e99dfcb128e24ea24d3a351311c9ac48d
tommatti <andre.rodrigues9911@gmail.com>
2025-06-15 00:24:09 -0300

    chore: changing frontend to vuejs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,14 +3,22 @@ pub mod api;
 pub mod models;
 pub mod ops;
 pub mod schema;
+use crate::ps_conec::update_migrations;
 
 #[macro_use] extern crate rocket;
 use api::{new_short_link_api, get_original_link_api};
+use rocket::figment::Figment;
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let _ = update_migrations();
+
+    let figment: Figment = Figment::from(rocket::Config::default())
+        .merge(("port", std::env::var("PORT").unwrap().parse::<u16>().unwrap()))
+        .merge(("address", "0.0.0.0"));
+
+    rocket::custom(figment)
         .mount("/", routes![new_short_link_api])
         .mount("/", routes![get_original_link_api])
 }
